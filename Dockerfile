d9a50925798f00544725f0cd63aa9385b28aa71d
jjy12163 <lalalove0310@gmail.com>
2026-02-21 11:54:00 +0900

    Feat: Cors 설정 추가

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,15 @@
-FROM python:3.9-slim
+FROM python:3.9-bullseye
 
 WORKDIR /app
 
-# Java 설치
 RUN apt-get update && apt-get install -y openjdk-11-jdk && apt-get clean
 
-# 환경변수 설정
 ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 ENV PATH=$JAVA_HOME/bin:$PATH
 
-# 필수 패키지 설치
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
-# 전체 소스 복사 (model_A, model_B 폴더 포함)
 COPY . .
 
-# 루트의 main.py 실행
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from fastapi import FastAPI
 from extraction_ai.routers.extraction_router import router as extraction_router
 from match_ai.routers.match_router import router as match_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+# 허용할 Origin 목록 (프론트엔드 주소)
+origins = [
+    "http://localhost:3000", 
+    "http://localhost:8080",   # 로컬 개발 환경
+    "http://http://52.78.20.212",   # 실제 서비스 도메인
+    "https://http://52.78.20.212",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # 모든 곳을 허용하려면 ["*"]
+    allow_credentials=True,
+    allow_methods=["*"],         # GET, POST, PUT, DELETE 등 모두 허용
+    allow_headers=["*"],         # 모든 헤더 허용
+)
 
 app.include_router(extraction_router, prefix="/fastapi")
 app.include_router(match_router, prefix="/fastapi")
